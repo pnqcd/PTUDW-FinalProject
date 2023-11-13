2c23eae8d5a758104178a9f2b156e5f44ee8fb7a
Quỳnh Phan <phanquynh437@gmail.com>
2023-11-13 20:30:46 +0700

    Change port

--- Express.js
+++ Express.js
@@ -109,7 +109,8 @@ app.get('/get-ad-details/:id', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
