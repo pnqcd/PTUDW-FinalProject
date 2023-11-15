d13b2fbb309ce97c1162d696ef5d279e4bf7de38
Quỳnh Phan <phanquynh437@gmail.com>
2023-11-15 12:48:55 +0700

    Add view detail board

--- soVHTT/script.js
+++ soVHTT/script.js
@@ -1,6 +1,23 @@
 function openCustomDown(elm) {
-    if (elm.parentElement.children[1].style.display == "none")
-        elm.parentElement.children[1].style.display = "block";
+    if (elm.parentElement.querySelector('.customDown').style.display === "none")
+        elm.parentElement.querySelector('.customDown').style.display = "block";
     else
-        elm.parentElement.children[1].style.display = "none";
+        elm.parentElement.querySelector('.customDown').style.display = "none";
+}
+
+function openViewDetail(elm) {
+    let div = document.createElement('div');
+    div.classList.add('modal-backdrop', 'fade', 'show');
+    document.body.appendChild(div);
+
+    elm.parentElement.parentElement.querySelector('.modal').classList.add('show');
+    elm.parentElement.parentElement.querySelector('.modal.detail-ward').style.display = "block";
+
+    elm.parentElement.style.display = "none";
+}
+
+function closeViewDetail(elm) {
+    elm.closest('.modal.detail-ward').classList.remove('show');
+    elm.closest('.modal.detail-ward').style.display = "none";
+    document.querySelector('.modal-backdrop.fade.show').remove();
 }
